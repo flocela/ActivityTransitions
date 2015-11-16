8e5efc0ac50cd92cddfdfd9a174d1809b72fcada
Flo B Jones <flocela@gmail.com>
2015-11-16 16:48:41 -0600

    circular_reveal done.

--- app/src/main/java/flobee/cognatesproofofconcept/ActivityAA.java
+++ app/src/main/java/flobee/cognatesproofofconcept/ActivityAA.java
@@ -6,11 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.transition.Slide;
 import android.view.Gravity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 public class ActivityAA extends Activity {
 
@@ -31,50 +28,15 @@ public class ActivityAA extends Activity {
         startActivity(i, transitionActivityOptions.toBundle());
       }
     });
-    final TextView bottomText = (TextView)findViewById(R.id.bottom_text);
-    bottomText.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        Intent i = new Intent(ActivityAA.this, ActivityBB.class);
-
-        View sharedView = indigoDot;
-
-        ActivityOptions transitionActivityOptions = ActivityOptions.makeSceneTransitionAnimation(ActivityAA.this, sharedView, "dot_transition");
-        startActivity(i, transitionActivityOptions.toBundle());
-      }
-    });
   }
 
   private void setupWindowAnimations() {
     // Re-enter transition is executed when returning to this activity
     Slide slideTransition = new Slide();
     slideTransition.setSlideEdge(Gravity.LEFT);
-    slideTransition.setDuration(600);
+    slideTransition.setDuration(500);
     getWindow().setReenterTransition(slideTransition);
     getWindow().setExitTransition(slideTransition);
   }
 
-
-
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.menu_main, menu);
-    return true;
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings) {
-      return true;
-    }
-
-    return super.onOptionsItemSelected(item);
-  }
 }
--- app/src/main/java/flobee/cognatesproofofconcept/ActivityBB.java
+++ app/src/main/java/flobee/cognatesproofofconcept/ActivityBB.java
@@ -8,8 +8,6 @@ import android.os.Bundle;
 import android.transition.Fade;
 import android.transition.Transition;
 import android.transition.TransitionInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewAnimationUtils;
 import android.view.animation.AccelerateInterpolator;
@@ -26,7 +24,7 @@ public class ActivityBB extends Activity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.ending_activity);
-    final TextView endingBottomTextView = (TextView)findViewById(R.id.more_info_from_bottom);
+    final TextView endingBottomTextView = (TextView)findViewById(R.id.paragraph);
     setupWindowAnimations();
     endingBottomTextView.setOnClickListener(new View.OnClickListener() {
       @Override
@@ -123,25 +121,4 @@ public class ActivityBB extends Activity {
     anim.start();
   }
 
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.menu_main, menu);
-    return true;
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings) {
-      return true;
-    }
-
-    return super.onOptionsItemSelected(item);
-  }
 }
